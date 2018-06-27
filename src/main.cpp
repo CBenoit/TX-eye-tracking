7e6210504c150cb9fc93b3d5a44c3d637364302c
Lucas Lazare <lazarelucas@yahoo.fr>
2018-06-27 22:22:37 +0200

    Temporary [for demo]

--- src/main.cpp
+++ src/main.cpp
@@ -7,36 +7,106 @@
 #include <cmath>
 #include <matrix.hpp>
 #include <eye_finder.hpp>
-#include <gaze_tracker.hpp>
 
-void process(const matrix<unsigned char>& pic, face f);
+std::optional<bool> process(const matrix<unsigned char>& pic, face f);
 
 int main(int, const char*[]) {
 
 	matrix<unsigned char> frame;
-	gaze_tracker gaze("res/haarcascade_frontalface_alt.xml");
+	eye_finder ef("res/haarcascade_frontalface_alt.xml");
 
 	cv::VideoCapture capture{0};
 	if (capture.isOpened()) {
-		gaze.configure("Configuring", capture);
+		capture.read(frame);
+		cv::flip(frame, frame, 1);
+		std::optional<face> f = ef.find_eyes(frame);
 		while (true) {
 			capture.read(frame);
-			cv::flip(frame, frame, 1);
-			matrix<unsigned char> copy(frame.clone());
-			auto g = gaze.track(copy);
-			if (g) {
-				std::cout << g->x << " - " << g->y << '\n';
-				cv::circle(frame, *g, 3, CV_RGB(40,40,200), 2);
-			} else {
-				cv::blur(frame, frame, cv::Size(20, 20));
-			}
-			cv::imshow("demo", frame);
-			if (cv::waitKey(1) == 27) {
-				exit(0);
-			}
-		}
 
+			// mirror it
+			cv::flip(frame, frame, 1);
+
+			// Apply the classifier to the frame
+			if (!frame.empty()) {
+				f = ef.find_eyes(frame, f);
+				if (f) {
+					std::optional<bool> is_left = process(frame, f.value());
+				}
+			} else {
+				printf(" --(!) No captured frame -- Break!");
+				break;
+			}
+
+			// escape quits
+//			int c = cv::waitKey(1);
+//			if ((char) c == 27) { break; }
+
+		}
 	}
 
 	return 0;
+}
+
+std::optional<bool> process(const matrix<unsigned char>& pic, face f) {
+
+	static std::pair<cv::Point,cv::Point> corners[4];
+	static std::optional<uint> to_draw;
+	static unsigned int count = 0;
+
+//	cv::Rect rects[] =
+//			{
+//					{0,0, pic.width() / 2, pic.height() / 2},
+//					{0,pic.height() / 2, pic.width() / 2, pic.height() / 2},
+//					{pic.width() / 2, pic.height() / 2, pic.width() / 2, pic.height() / 2},
+//					{pic.width() / 2, 0, pic.width() / 2, pic.height() / 2},
+//			};
+
+	char t = static_cast<char>(cv::waitKey(1));
+	if (t == ' ') {
+		if (count < 4) {
+			corners[count++] = {f.eyes.first.eye_position, f.eyes.second.eye_position};
+		}
+	} else if (t == 27) {
+        exit(0);
+    }
+
+
+	f.eyes.first.eye_position.x += f.face_region.x;
+	f.eyes.first.eye_position.y += f.face_region.y;
+	f.eyes.second.eye_position.x += f.face_region.x;
+	f.eyes.second.eye_position.y += f.face_region.y;
+
+	matrix<unsigned char> display(pic.clone());
+	cv::rectangle(display, f.face_region, CV_RGB(40,40,200));
+
+//	if (to_draw) {
+//		cv::rectangle(display, rects[to_draw.value()], CV_RGB(40,220,40), -1);
+//	}
+
+	cv::rectangle(display, f.face_region, CV_RGB(200, 0, 200));
+	cv::circle(display, f.eyes.first.eye_position, 2, CV_RGB(40,40,200), 2);
+	cv::circle(display, f.eyes.second.eye_position, 2, CV_RGB(200,40,40), 2);
+
+	cv::imshow("aiue", display);
+
+	if (count >= 4) {
+		double min = std::numeric_limits<double>::infinity();
+		for (auto i = 0u ; i < 4 ; ++i) {
+			double dist = std::hypot(f.eyes.first.eye_position.x - corners[i].first.x,
+			                         f.eyes.first.eye_position.y - corners[i].first.y);
+			dist += std::hypot(f.eyes.second.eye_position.x - corners[i].second.x,
+			                   f.eyes.second.eye_position.y - corners[i].second.y);
+
+			if (dist < min) {
+				min = dist;
+				to_draw = i;
+			}
+		}
+	}
+
+	if (to_draw) {
+		return to_draw.value() < 2;
+	} else {
+		return {};
+	}
 }
